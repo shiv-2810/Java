d1861427ee8a1f085e0ba12d7eeff512d8c398a1
shiv-2810 <s9431494772@gmail.com>
2022-01-24 09:37:13 +0530

    creating first ArrayList

--- arrayList.java
+++ arrayList.java
@@ -1,6 +1,20 @@
+import java.util.ArrayList;
+
 public class arrayList {
     public static void main(String[] args) {
-        System.out.println("HI shivam");
-    }
+        ArrayList<Integer>li=new ArrayList<>(5);
+        li.add(45);
+        li.add(43);
+        li.add(45);
+        li.add(43);
+        li.add(45);
+        li.add(43);
+        li.add(45);
+        li.add(43);
+        li.add(45);
+        li.add(43);
+
+        System.out.println(li);
+       }
 }
 
